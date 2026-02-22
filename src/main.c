804ce83411ff3ff653f729cde0216b460f445683
BalakeKarbon <blakekarbon@gmail.com>
2026-02-22 00:37:40 +0000

    Working on proper 6502/C128 target.
    The last version was just for testing syntax and such. Now we are actually building a 6502 target in this case for the Commodore 128. This is only chosen because its the highest memory system emulated by VICE.

--- src/main.c
+++ src/main.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
-
+#include <stdint.h>
 int main() {
-	printf("Hellorld!\n");
-	int i = 1;
-	long l = 1;
-	int ic = 0;
-	int lc = 0;
-	while (i != 0) {
-		ic++;
-		i <<= 8;
-	}
-	while (l != 0) {
-		lc++;
-		l <<=8;
-	}
-	printf("Size of int: %i bytes.\n", ic);
-	printf("Size of long: %i bytes.\n", lc);
+	uint8_t test = 2;
 	return 0;
 }
