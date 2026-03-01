349556665138404b45e2f5dedc16d01aa4a10154
BalakeKarbon <blakekarbon@gmail.com>
2026-03-01 02:08:28 +0000

    Much learning and planning.
    Realization of how optimized we need to be for the example but how flexible we would like this to be for future expirimentation and play

--- src/ann.h
+++ src/ann.h
@@ -4,6 +4,16 @@
 #include "math.h"
 #include <stdint.h>
 
+/* Okay so this library should be able to implement any optimizations we may need.
+ * Specifically for our 8088 machine we want parts that are 16 bit fixed point
+ * and 8 bit fixed point, etc, but we also want to support experimentation. This
+ * means the ability to compile LUT's or do math in time, this means differing
+ * fixed point values that are configurable during build, start with a bare bones
+ * capability targeted at our system but allow crazy stuff like 64 bit fixed
+ * point maybe even floating point. I would not worry about threading yet though?.
+ * An impractically flexible NN library.
+ */
+
 // Implement a directed cyclic graph
 
 
--- src/fixedpoint.h
+++ src/fixedpoint.h
@@ -3,34 +3,47 @@
 
 #include <stdint.h>
 
-#ifndef Q_BASE_TYPE
-#define Q_BASE_TYPE int16_t
-#endif
+/* It looks like were really going to have to specialize so:
+ * LUT for sigmoid and tanh
+ * 8 bit values - Q2.5 probably (1 sign bit) for values providing 8 bit multiplication speed and a range of +- ~ 4?
+ *
+ * This is because multiplication is required outside of sigmoid and tanh and is much slower if we have to do 16 bit multiply.
+ * Hopefully 8 bit fixed point is precise enough...
+ */
 
-#ifndef Q_FRAC_BITS
-#define Q_FRAC_BITS 15
-#endif
-
-// Example of Q1.15:
-// #define Q_BASE_TYPE int16_t
-// #define Q_FRAC_BITS 15
-// Example of Q2.15:
-// #define Q_BASE_TYPE int16_t
-// #define Q_FRAC_BITS 14
-// Example of Q1.7:
-// #define Q_BASE_TYPE int8_t
-// #define Q_FRAC_BITS 7
-
-// Types we want:
-// Q1.15
-// Q2.14
-// Q1.7
+//// Can be set with compiler flags
+//#ifndef Q_BASE_TYPE
+//#define Q_BASE_TYPE int16_t
+//#endif
+//#ifndef Q_FRAC_BITS
+//#define Q_FRAC_BITS 15
+//#endif
+//// Example of Q1.15:
+//// #define Q_BASE_TYPE int16_t
+//// #define Q_FRAC_BITS 15
+//// Example of Q2.15:
+//// #define Q_BASE_TYPE int16_t
+//// #define Q_FRAC_BITS 14
+//// Example of Q1.7:
+//// #define Q_BASE_TYPE int8_t
+//// #define Q_FRAC_BITS 7
+//
+//typedef Q_BASE_TYPE q_t;
+//
+//#define Q_SCALE ((q_t)1 << Q_FRAC_BITS)
+//
+//// Types we want:
+//// Q1.15
+//// Q2.14
+//// Q1.7
 
 typedef struct {
 	// Fixed Point Stuff
 } FP;
 
+// Make lookup tables pre-computed for:
 // Tanh
 // Sigmoid
+// etc
 
 #endif
